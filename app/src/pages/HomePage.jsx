b72536fbd1620e2c0e7dbecc7776ddc412f44a9e
Jonathan Wang <j2279wan@edu.uwaterloo.ca>
2020-02-24 15:59:33 -0500

    Placeholder for HomePage

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,25 +1,29 @@
-import React, { useState, useEffect, useDebugValue } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Redirect } from 'react-router-dom'
 
 import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 import app from 'DMF/feathers-client.js'
+import useResponsive from 'DMF/hooks/useResponsive'
 
 const HomePage = props => {
   const [isAuthenticated, setIsAuthenticated] = useState(true)
 
+  const { onMobile } = props
+
   const handleLogOut = () => {
-    app.logout()
-      .then(() => setIsAuthenticated(false))
+    app.logout().then(() => setIsAuthenticated(false))
   }
 
   useEffect(() => {
-    app.authentication
-      .getAccessToken()
-      .then(accessToken => {
-        accessToken ? app.reAuthenticate().then(() => setIsAuthenticated(true)) : setIsAuthenticated(false)
-      })
+    app.authentication.getAccessToken().then(accessToken => {
+      accessToken
+        ? app.reAuthenticate().then(() => setIsAuthenticated(true))
+        : setIsAuthenticated(false)
+    })
   }, [])
 
   if (!isAuthenticated) {
@@ -27,12 +31,40 @@ const HomePage = props => {
   }
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button onClick={handleLogOut}>Log Out</Button>
-      <h2>{isAuthenticated ? 'Authed' : 'Not Authed'}</h2>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        overflow: 'hidden',
+        position: 'absolute'
+      }}>
+      <Paper
+        elevation={onMobile ? 0 : 3}
+        style={{
+          padding: onMobile ? 10 : 20,
+          position: 'relative',
+          minHeight: 472,
+          ...(onMobile
+            ? { height: '100%', width: '100%', overflow: 'scroll' }
+            : { width: 500 })
+        }}>
+        <Typography variant='h3' style={{ margin: '20px' }}>HOME PAGE</Typography>
+        <Typography variant='body1' style={{ margin: '10px' }}>
+          Testing testing 123
+        </Typography>
+        <Button
+          variant='contained'
+          color='default'
+          style={{ width: '100%' }}
+          onClick={handleLogOut}>
+          Log Out
+        </Button>
+      </Paper>
     </div>
   )
 }
 
-export default HomePage
+export default useResponsive()(HomePage)
